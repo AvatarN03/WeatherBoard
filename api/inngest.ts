c8b80eaf7ef5875705db439922260e80cb98afca
AvatarN03 <jsprashanth003@gmail.com>
2026-06-09 21:34:19 +0530

    fix: inngest function extension

--- api/inngest.ts
+++ api/inngest.ts
@@ -1,7 +1,7 @@
 import { serve } from "inngest/node";
 import { inngest } from "../inngest/client.js";
-import { sendWelcomeEmail } from "../inngest/functions/welcomeEmail.js";
-import { sendDailyDigest } from "../inngest/functions/sendDailyDigest.js";
+import { sendWelcomeEmail } from "../inngest/functions/welcomeEmail";
+import { sendDailyDigest } from "../inngest/functions/sendDailyDigest";
 
 const handler = serve({
   client: inngest,
